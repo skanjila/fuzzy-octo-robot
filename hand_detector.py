8cb9aa1a45a2dde5c490ff690f56d703939ff60a
Neil Macneale <neil.macneale.v@gmail.com>
2022-02-01 13:58:48 -0800

    Made cv/cv2 consistent, and simplified the loops a bit

--- hand_detector.py
+++ hand_detector.py
@@ -14,36 +14,36 @@
 # import the necessary packages
 # from picamera.array import PiRGBArray  # Generates a 3D RGB array
 # from picamera import PiCamera  # Provides a Python interface for the RPi Camera Module
-import time  # Provides time-related functions
-import cv2 as cv # OpenCV library
+
+import time
+import cv2
 import numpy as np
 import sys
 from skimage.metrics import structural_similarity as compare_ssim
 import imutils
 
-
-img = cv.imread(cv.samples.findFile("resources/background.png"))
-gray_background = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+img = cv2.imread(cv2.samples.findFile("resources/background.png"))
+gray_background = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 if img is None:
     sys.exit("Could not read the image.")
-cv.imshow("Display window", gray_background)
-# k = cv.waitKey(0)
+cv2.imshow("Display window", gray_background)
+# k = cv2.waitKey(0)
 
 WIN_RF = "Reference"
 
 framenum = -1 # Frame counter
-cap = cv.VideoCapture("sample1.h264")
+cap = cv2.VideoCapture("sample1.h264")
 
 if not cap.isOpened():
     print("Could not open the reference " + sourceReference)
     sys.exit(-1)
 
-ref_size = (int(cap.get(cv.CAP_PROP_FRAME_WIDTH)), int(cap.get(cv.CAP_PROP_FRAME_HEIGHT)))
-cv.namedWindow(WIN_RF, cv.WINDOW_AUTOSIZE)
-cv.moveWindow(WIN_RF, 0, 0) #750,  2 (bernat =0)
+ref_size = (int(cap.get(cv2.CAP_PROP_FRAME_WIDTH)), int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT)))
+cv2.namedWindow(WIN_RF, cv2.WINDOW_AUTOSIZE)
+cv2.moveWindow(WIN_RF, 0, 0) #750,  2 (bernat =0)
 
-# cv.namedWindow("diff", cv.WINDOW_AUTOSIZE)
-# cv.moveWindow("diff", 640, 0) #750,  2 (bernat =0)
+# cv2.namedWindow("diff", cv2.WINDOW_AUTOSIZE)
+# cv2.moveWindow("diff", 640, 0) #750,  2 (bernat =0)
 
 def skeletonize(image_in):
     '''Inputs and grayscale image and outputs a binary skeleton image'''
@@ -72,26 +72,24 @@ delay = 3
 while(cap.isOpened()):
     # Capture frame-by-frame
     ret, frame = cap.read()
-    if ret == True:
-        gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+    if ret:
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
         out = gray.copy()
         out[0:0, 300:400] = 0
         # diff = gray - gray_background
-        cv.subtract(gray, gray_background, out)
+        cv2.subtract(gray, gray_background, out)
 
         kernel = np.ones((5, 5), np.float32) / 25
-        dst = cv.filter2D(out, -1, kernel)
+        dst = cv2.filter2D(out, -1, kernel)
 
-        ret, thresh1 = cv.threshold(dst, 25, 255, cv.THRESH_BINARY)
+        ret, thresh1 = cv2.threshold(dst, 25, 255, cv2.THRESH_BINARY)
 
         ## Using SSIM Instead of our more manual approach
         (score, diff) = compare_ssim(gray, gray_background, full=True)
         diff = (diff * 255).astype("uint8")
         print("SSIM: {}".format(score))
-        thresh = cv.threshold(diff, 0, 255,
-                               cv.THRESH_BINARY_INV | cv.THRESH_OTSU)[1]
-        cnts = cv.findContours(thresh.copy(), cv.RETR_EXTERNAL,
-                                cv.CHAIN_APPROX_SIMPLE)
+        thresh = cv2.threshold(diff, 0, 255, cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU)[1]
+        cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
         cnts = imutils.grab_contours(cnts)
 
         # loop over the contours
@@ -99,29 +97,26 @@ while(cap.isOpened()):
             # compute the bounding box of the contour and then draw the
             # bounding box on both input images to represent where the two
             # images differ
-            (x, y, w, h) = cv.boundingRect(c)
-            cv.rectangle(gray, (x, y), (x + w, y + h), (0, 0, 255), 2)
-            # cv.rectangle(imageB, (x, y), (x + w, y + h), (0, 0, 255), 2)
+            (x, y, w, h) = cv2.boundingRect(c)
+            cv2.rectangle(gray, (x, y), (x + w, y + h), (0, 0, 255), 2)
+            # cv2.rectangle(imageB, (x, y), (x + w, y + h), (0, 0, 255), 2)
 
         # Display the resulting frame
-        cv.imshow('Frame', gray)
-        cv.imshow("diff", thresh1)
+        cv2.imshow('Frame', gray)
+        cv2.imshow("diff", thresh1)
 
         # Press Q on keyboard to  exit
-        if cv.waitKey(250) & 0xFF == ord('q'):
+        if cv2.waitKey(250) & 0xFF == ord('q'):
             break
-
-    # Break the loop
     else:
         break
 
 
 # Press Q on keyboard to  exit
-cv.waitKey(0)
+cv2.waitKey(0)
 
 # When everything done, release the video capture object
 cap.release()
 
 # Closes all the frames
-cv.destroyAllWindows()
-
+cv2.destroyAllWindows()
